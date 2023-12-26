94ff3da20ad4334227490c6d8f0607e9b4585997
jun447 <junaidmumtaz16@gmail.com>
2023-12-26 11:29:00 +0500

    project completed

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+//
+// app.use("/auth", userRouter);
+// app.use("/recipes", recipesRouter);
+//    "start": "NODE_OPTIONS='--experimental-specifier-resolution=node' nodemon src/index.js"
+mongoose.connect(
+  "mongodb+srv://devjun:devjun123@recipes.w9ptk6h.mongodb.net/recipes?retryWrites=true&w=majority",
+  // {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // }
+);
+
+app.listen(3001, () => console.log("Server started"));
